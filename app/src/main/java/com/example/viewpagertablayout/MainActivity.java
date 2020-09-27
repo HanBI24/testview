7c509c40593357379b47e4dbcd6b67499b62c7e5
HanBI24 <alsltjdrhs@naver.com>
2020-09-28 01:30:15 +0900

    test e

---
+++ app/src/main/java/com/example/viewpagertablayout/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.viewpagertablayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toolbar;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+    ViewPager2 pager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pager = (ViewPager2)findViewById(R.id.pager);
+        pager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+        pager.setOffscreenPageLimit(3);
+
+        MyPagerAdapter adapter = new MyPagerAdapter(getSupportFragmentManager(), getLifecycle());
+        pager.setAdapter(adapter);
+
+    }
+
+    class MyPagerAdapter extends FragmentStateAdapter {
+        int itemCnt = 3;
+
+        public MyPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+            super(fragmentManager, lifecycle);
+        }
+
+        @Override
+        public int getItemCount() {
+            return itemCnt;
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch(position) {
+                case 0: {
+                    return new Fragment1();
+                }
+                case 1: {
+                    return new Fragment2();
+                }
+                case 2: {
+                    return new Fragment3();
+                }
+            }
+            return new Fragment1();
+        }
+    }
+}
